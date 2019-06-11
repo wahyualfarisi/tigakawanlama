0ad126e9842f6f9a5c45f1b9536048fba78c7479
wahyu alfarisi <wahyualfarisi@gmail.com>
2019-06-11 13:03:21 +0700

    initialize app

---
+++ application/models/M_core.php
@@ -0,0 +1,34 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class M_core extends CI_Model {
+
+
+    function add_data($table, $data)
+    {
+        return $this->db->insert($table, $data);
+    }
+
+    function get_order_where_list($table, $where, $fieldOrder, $methodOrder)
+    {
+        $this->db
+            ->select('*')
+            ->from($table)
+            ->where($where)
+            ->order_by($fieldOrder, $methodOrder);
+            return $this->db->get();
+    }
+
+    function get_all_table()
+    {
+        $this->db
+             ->select('*')
+             ->from($table)
+             ->order_by($fieldOrder, $methodOrder);
+            return $this->db->get();
+    }
+
+
+
+
+}
